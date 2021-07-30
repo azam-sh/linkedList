7fa129f8166d97726af5485aaf29bd5d5ed87093
Azam <azam.sh3603@gmail.com>
2021-07-30 20:29:41 +0500

    adding test

---
+++ main_test.go
@@ -0,0 +1,14 @@
+package main
+
+import "testing"
+
+func TestListAdd(t *testing.T) {
+	q := List{}
+
+	if q.length != 0 {
+		want := 0
+		got := q.len()
+		t.Errorf("method len() in method (add) is not correct for empty queue got %d want %d/n", got, want)
+	}
+	// TODO: TESTs for new methods
+}
